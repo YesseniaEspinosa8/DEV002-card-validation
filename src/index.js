3caf657c2291d505f9fc342d1e261624f4d75e05
YesseniaEspinosa8 <yeye_espinosa@outlook.com>
2022-11-17 08:13:03 -0600

    Agregue imagenes y le di stilo css a mi pagina

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-import validator from './validator.js';
-
-console.log(validator);
+function tarjeta(numero) {
+    Number = Number.toString()
+    acumulador = ""
+    for (let i = 0; i<Number.length;i++){
+  if(i<4){
+  
+    acumulador = acumulador + Number  [1]
+  } else {
+    acumulador = acumulador + "#"
+  
+  
+  }
+  
+  
+    }
+    return acumulador
+  
+    
+  }
+  tarjeta(4152313843065579)
+  
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,21 @@
-const validator = {
-  // ...
-};
 
-export default validator;
+function tarjeta(numero) {
+  Number = Number.toString()
+  acumulador = ""
+  for (let i = 0; i<Number.length;i++){
+if(i<4){
+
+  acumulador = acumulador + Number  [1]
+} else {
+  acumulador = acumulador + "#"
+
+
+}
+
+
+  }
+  return acumulador
+
+  
+}
+tarjeta(4152313843065579)
