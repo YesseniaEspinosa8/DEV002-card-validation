77a72f9e3988127875ec7faa90dd884777ac02ee
YesseniaEspinosa8 <yeye_espinosa@outlook.com>
2022-12-03 23:50:25 -0600

    Volvi a revisar el algoritmo de luhn

--- src/validator.js
+++ src/validator.js
@@ -1,58 +1,45 @@
 const validator = {
-  isValid: function (tarjeta) {
-
-    let sum = 0;
-    //Validar si es que entra un numero
-
-    if (tarjeta === null || (isNaN(tarjeta) === true)) {
-      alert('Ingrese un número de tarjeta válido');
-
-    }
+  isValid: (creditNumber) => {
+    const length = creditNumber.length; //longitud del string 
+    let agregarDigitos = 0;
 
     //Pasar los numeros a un array en orden inverso
-    const arrayNumber = tarjeta.split("");
+    const arrayNumber = creditNumber.split("");
     console.log(arrayNumber)
     const arrayReverse = arrayNumber.reverse();
     console.log(arrayReverse)
 
-    //Aplicar la operacion a los numeros de las pocisiones pares
+    // si la cantidad  de digitos, al modulo(%) 2 da como resultado 0
+    if (length % 2 == 0) {
+      // Comienza al inicio del numero y duplica a partir del primero numero
+      for (let i = 0; i < length; i++) {
+        let digitoActual = parseInt(creditNumber[i]);
+        if (i % 2 == 0)// tomar posiciones pares
+        // si el digito actual multiplicado x 2 es mayor que 9
+        {
+          if ((digitoActual * 2) > 9) {
+            //Separar los valores de 2 digitos para despues sumarlos 
+            let primerNumero = parseInt(digitoActual / 10);
+            let ultimoNumero = digitoActual % 10;
+            digitoActual = primerNumero + ultimoNumero;
 
-    for (let i = 0; i < arrayNumber; i++) {
-      let digitoActual = parseInt(tarjeta[i]);
-      if (i % 2 == 0) // tomar posiciones pares
-      {
-        if ((digitoActual *= 2) > 9) {
-          //Separar los valores de 2 digitos para despues sumarlos
-          let primerNumero = parseInt(digitoActual / 10);
-          let ultimoNumero = digitoActual % 10;
-          digitoActual = primerNumero + ultimoNumero;
+          }
         }
+        // Suma todos los digitos de la tarjeta 
+        agregarDigitos += digitoActual; // Consolidar agregarDigitos / Que pasa con los impares y menores a 9?
       }
-      //Sumar todos los digitos de la tarjeta que se ingresa
-      agregarDatos += digitoActual;
     }
-
-
-
-
-    //Comprobar si es una tarjeta válida
-    if (sum % 10 === 0 && sum !== 0) {
-      return alert('Datos ingresados correctamente. !Tarjeta válida!');
-    }
-    else {
-      return alert('Tarjeta inválida');
-    }
+    // creo otra constante para alojar la suma de los digitos, ver si el total sacando su modulo es === a 0,
+    // al ser un typeOf retorna un tipo de dato primitivo, si no es true, entonces devuelve false
+    const total = (agregarDigitos % 10) === 0
+    console.log(total, agregarDigitos)
+    return total
 
 
   },
-  maskify: function (tarjeta) {
-    console.log(tarjeta);
 
-    maskify("3753878556798751"); // '############8751'
-    maskify("3753878556798751"); // '#######8751'
-    maskify("1"); // '1'
-    maskify(""); // ''
-  }
+
+  maskify: (creditNumber) => { }
 
 
 }
