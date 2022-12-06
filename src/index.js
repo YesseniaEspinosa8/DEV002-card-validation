16171ef0868808ce74a532b6d2b13e7dc969c8fe
YesseniaEspinosa8 <yeye_espinosa@outlook.com>
2022-12-06 15:20:57 -0600

    Reeorganice mi codigo en validator.js, termine maskify y aplique los test

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import validator from "./validator.js";
 
 const btnSubmit = document.getElementById("btnEnviar");
-const imputCard = document.getElementById ("inputNumeroDeTarjeta");
+const imputCard = document.getElementById("inputNumeroDeTarjeta");
 
 btnSubmit.addEventListener("click", function () {
-const numberCard = imputCard.value;
-console.log (numberCard)
-validator.isValid(numberCard)   
+    const numberCard = imputCard.value;
+    console.log(numberCard)
+    const resultMaskify = validator.maskify(numberCard) 
+    const resultValid = validator.isValid(numberCard)
+    if (resultValid) {
+        alert("Éxito, tu tarjeta " + resultMaskify +" es válida")
+    } else {
+        alert("Tarjeta " + resultMaskify +" inválida")
+
+    }
+    validator.maskify(numberCard) 
+
+
+
 })
 
 
-  
--- src/validator.js
+++ src/validator.js
@@ -3,51 +3,65 @@ const validator = {
     const length = creditNumber.length;
     let agregarDigitos = 0;
 
-    //Pasar los numeros a un array en orden inverso
-    const arrayNumber = creditNumber.split("");
-    console.log(arrayNumber)
-    const arrayReverse = arrayNumber.reverse();
-    console.log(arrayReverse)
+    
 
-    // si la cantidad  de digitos, al modulo(%) 2 da como resultado 0
-    if (length % 2 == 0) {
+    // si la cantidad  de digitos, al sacar modulo(%) 2 da como resultado 0
+    // if (length % 2 == 0) {
       // Comienza al inicio del numero y duplica a partir del primero numero
       for (let i = 0; i < length; i++) {
         let digitoActual = parseInt(creditNumber[i]);
         if (i % 2 == 0)// tomar posiciones pares
+
         // si el digito actual multiplicado x 2 es mayor que 9
-        {
-          if ((digitoActual * 2) > 9) {
+        { console.log(digitoActual)
+          if ((digitoActual *= 2) > 9) {
+            console.log ("numero x 2" + digitoActual)
             //Separar los valores de 2 digitos para despues sumarlos 
             let primerNumero = parseInt(digitoActual / 10);
             let ultimoNumero = digitoActual % 10;
+            //sumar el primer y ultimo numero
             digitoActual = primerNumero + ultimoNumero;
+            console.log("Numero x 2 sumado" + digitoActual)
 
           }
-        }
+        } console.log("impares" + digitoActual)
         // Suma todos los digitos de la tarjeta 
         agregarDigitos += digitoActual; // Consolidar agregarDigitos / Que pasa con los impares y menores a 9?
+        console.log("suma De Numeros  " + agregarDigitos)
       }
-    }
+    //} 
+    
+
     // creo otra constante para alojar la suma de los digitos, ver si el total sacando su modulo es === a 0,
     // al ser un typeOf retorna un tipo de dato primitivo, si no es true, entonces devuelve false
     const total = (agregarDigitos % 10) === 0
     console.log(total, agregarDigitos)
-    return total
+    if (total) {
+      return true
 
+    }
+    else {
+      return false
 
+    }
 
   },
 
+
+
   maskify: (creditNumber) => {
-    return cc.replace(/.(?=.{4})/g, "#");
+    console.log(creditNumber.replace(/.(?=.{4})/g, "#"));
+    return creditNumber.replace(/.(?=.{4})/g, "#");
+
 
   }
 
 }
-
 export default validator;
-const resultado = window.confirm("Ingreso exitoso. Ya puedes pagar tu producto");
+
+
+
+
 
 
 
